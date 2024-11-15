2d4db2c7c600f3ecddae6316a54cba998a7713aa
William Jackson <565174+williamjacksn@users.noreply.github.com>
2024-11-15 15:13:12 -0600

    Calculate next string instead of starting over

--- python/hashbrowns.py
+++ python/hashbrowns.py
@@ -1,13 +1,31 @@
 import argparse
 import hashlib
-import itertools
+
+ALPHABET = '+/0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+CHAR_IDX = {char: idx for idx, char in enumerate(ALPHABET)}
+
+def get_next_string(s: str = None) -> str:
+    if not s:
+        return '+'
+
+    chars = list(s)
+    i = len(chars) - 1
+    while i >= 0 and chars[i] == 'z':
+        chars[i] = '+'
+        i -= 1
+    if i < 0:
+        return '+' + ''.join(chars)
+
+    current_index = CHAR_IDX[chars[i]]
+    chars[i] = ALPHABET[current_index + 1]
+    return ''.join(chars)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--username', default='williamjackson')
     parser.add_argument('-l', '--length', type=int, default=1)
-    parser.add_argument('-s', '--start', default='')
+    parser.add_argument('-s', '--start', default='+')
     parser.add_argument('-b', '--best', default='f'*64)
     return parser.parse_args()
 
@@ -18,18 +36,19 @@ def format_sha(sha: str) -> str:
 
 def main():
     args = parse_args()
-    base64_chars = '+/0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
     best = args.best
-    for i, x in enumerate(itertools.product(base64_chars, repeat=args.length)):
-        suffix = ''.join(x)
+    i = 0
+    suffix = args.start
+    while True:
+        i += 1
         value = f'{args.username}/{suffix}'
         if i % 500000 == 0:
             print(value, end='\r')
-        if suffix > args.start:
-            sha = hashlib.sha256(value.encode()).hexdigest()
-            if sha < best:
-                best = sha
-                print(value, format_sha(sha))
+        sha = hashlib.sha256(value.encode()).hexdigest()
+        if sha < best:
+            best = sha
+            print(value, format_sha(sha))
+        suffix = get_next_string(suffix)
 
 
 if __name__ == '__main__':
